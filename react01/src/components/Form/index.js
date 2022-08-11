34d958521f7c807cf2790bccfa80e72b2bf6bd04
Vinícius Soares <vinivsp2304@hotmail.com>
2022-08-11 16:06:31 -0300

    Add: Rota post => formulario é enviado para o banco de dados

--- react01/src/components/Form/index.js
+++ react01/src/components/Form/index.js
@@ -1,22 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import api from "../../service/api";
 import Perguntar from "../Button/Perguntar";
 import { Card, FormModel, Input, Label, TextArea } from "./style";
 
 const Form = () => {
+  const [pergunta, setPergunta] = useState({
+    titulo: "",
+    descricao: "",
+  });
+
+  const handlePergunta = (e) => {
+    setPergunta((old) => {
+      return {
+        ...old,
+        [e.target.name]: e.target.value,
+      };
+    });
+  };
+
   return (
     <Card>
       <FormModel>
         <Label>Título</Label>
-        <Input type="text" name="titulo" placeholder="Título da pergunta" />
+        <Input
+          onChange={handlePergunta}
+          value={pergunta.titulo}
+          type="text"
+          name="titulo"
+          placeholder="Título da pergunta"
+        />
         <Label>Descrição</Label>
         <TextArea
+          onChange={handlePergunta}
+          value={pergunta.descricao}
           name="descricao"
           placeholder="Descreva aqui sua duvida para que alguém possa te ajudar"
         />
         <Perguntar
           onClick={(e) => {
-            console.log("teste");
-            e.preventDefault(); // previne/impede um evento padrão | nesse caso, impede que a página atualize
+            api
+              .post("/salvarpergunta", pergunta)
+              .then((response) => alert(response.data));
+            // e.preventDefault();
           }}
         />
       </FormModel>
